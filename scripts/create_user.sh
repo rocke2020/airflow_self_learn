1c95613600118e119d31a7d2093b7a0bc4d450f7
qichang.dong <dongqichang@ecmas.ai>
2024-11-18 08:09:25 +0800

    u

---
+++ scripts/create_user.sh
@@ -0,0 +1,7 @@
+airflow users create \
+    --username rocked \
+    --firstname Rocke \
+    --lastname dong \
+    --role Admin \
+    -p 123qwe.. \
+    --email qc_dong@126.com
---
+++ scripts/db_migrate.sh
@@ -0,0 +1,2 @@
+export AIRFLOW__DATABASE__SQL_ALCHEMY_CONN=mysql+mysqldb://root:123qwe..@127.0.0.1:3306/airflow_db
+airflow db migrate
